bfb9086a0d9a89e4ff435b2b416c7a926fc72437
Theodore Cleveland <tcclevela@gmail.com>
2013-07-01 16:28:11 -0400

    moved around table initialization order to deal with id dependencies and created tables

--- tables.sql
+++ tables.sql
@@ -1,17 +1,13 @@
-DROP TABLE tasks;
-DROP TABLE movies;
-DROP TABLE people;
+-- DROP TABLE tasks;
+-- DROP TABLE movies;
+-- DROP TABLE people;
 
-CREATE TABLE tasks
+
+CREATE TABLE people
 (
 	id SERIAL PRIMARY KEY,
-	task VARCHAR(255),
-	details VARCHAR(255),
-	due VARCHAR(30),
-	urgent BOOLEAN,
-	complete BOOLEAN DEFAULT FALSE,
-	director_id INT REFERENCES people(id)
-	movie_id INT REFERENCES movies(id)
+	name VARCHAR(255),
+	email VARCHAR(255)
 );
 
 CREATE TABLE movies
@@ -22,9 +18,15 @@ CREATE TABLE movies
 	director_id INT REFERENCES people(id)
 );
 
-CREATE TABLE people
+CREATE TABLE tasks
 (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(255),
-	email VARCHAR(255)
+	task VARCHAR(255),
+	details VARCHAR(255),
+	due VARCHAR(30),
+	urgent BOOLEAN,
+	complete BOOLEAN DEFAULT FALSE,
+	director_id INT REFERENCES people(id),
+	movie_id INT REFERENCES movies(id)
 );
+
