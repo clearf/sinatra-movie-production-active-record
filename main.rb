b353d8046f8ac449cc729fc819d1ae9afd33bd4c
egong <egong82@gmail.com>
2013-07-01 17:12:28 -0400

    created tables

--- main.rb
+++ main.rb
@@ -2,6 +2,25 @@ require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+def run_sql(sql)
+  db = PG.connect(dbname: 'movie_app', host: 'localhost')
+  result = db.exec(sql)
+  db.close
+  result
+end
+
 get '/' do
 erb :index
+end
+
+get '/todos' do
+erb :todo
+end
+
+get '/movies' do
+erb :movies
+end
+
+get '/people' do
+erb :people
 end
---
+++ movieapp.sql
@@ -0,0 +1,28 @@
+DROP TABLE todo;
+DROP TABLE people;
+DROP TABLE movies;
+
+CREATE TABLE todo
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  contact VARCHAR(255),
+  movie VARCHAR(255)
+);
+
+
+CREATE TABLE people
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  movie VARCHAR(255),
+  task VARCHAR(255)
+);
+
+CREATE TABLE movies
+(
+  release_date VARCHAR(255),
+  director VARCHAR(255)
+);
+
