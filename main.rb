23243668cadcca17f937875ae10399568664828e
Theodore Cleveland <tcclevela@gmail.com>
2013-07-01 16:31:04 -0400

    created helper sql executor function

--- main.rb
+++ main.rb
@@ -3,6 +3,16 @@ require 'pg'
 require 'postgres'
 require 'postgres/reloader' if development?
 
+helpers do 
+	def exec_sql(sql)
+		db = PG.connect(dbname: 'production', host: 'localhost')
+		result = db.exec(sql)
+		db.close
+		result
+	end
+end
+
+
 get '/' do
 
 end
