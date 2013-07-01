8054f9c78934f91895b82424c99a5d616e10c61f
Christine Coulter <christinetcoulter@gmail.com>
2013-07-01 17:31:33 -0400

    Change tables in movie_production.sql

--- movie_production.sql
+++ movie_production.sql
@@ -1,20 +1,20 @@
-DROP TABLE movies;
-DROP TABLE people;
-DROP TABLE tasks;
-
-
-CREATE TABLE people
-(
-  id SERIAL PRIMARY KEY,
-  person_name VARCHAR(255)
-);
+-- DROP TABLE movies;
+-- DROP TABLE people;
+-- DROP TABLE tasks;
 
 CREATE TABLE movies
 (
   id SERIAL PRIMARY KEY,
   movie_name VARCHAR(255),
-  release_date VARCHAR(255),
-  director_id INT REFERENCES people(id)
+  release_date VARCHAR(255)
+);
+
+CREATE TABLE people
+(
+  id SERIAL PRIMARY KEY,
+  person_name VARCHAR(255),
+  role VARCHAR(255),
+  movie_name INT REFERENCES movies(id)
 );
 
 CREATE TABLE tasks
@@ -22,6 +22,9 @@ CREATE TABLE tasks
   id SERIAL PRIMARY KEY,
   task_name VARCHAR(255),
   description VARCHAR(255),
-  movie_id INT REFERENCES movies(id),
-  person_id INT REFERENCES people(id)
+  person_id INT REFERENCES people(id),
+  movie_name INT REFERENCES movies(id)
 );
+
+-- make a person
+-- INSERT INTO people (person_name)
