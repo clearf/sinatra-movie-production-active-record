379802dd91442ab61d858e19f5ea18542a2f5271
Christine Coulter <christinetcoulter@gmail.com>
2013-07-01 17:48:23 -0400

    Replace with role with director for people table in movie_production.sql file

--- movie_production.sql
+++ movie_production.sql
@@ -13,7 +13,7 @@ CREATE TABLE people
 (
   id SERIAL PRIMARY KEY,
   person_name VARCHAR(255),
-  role VARCHAR(255),
+  director BOOLEAN,
   movie_name INT REFERENCES movies(id)
 );
 
@@ -26,5 +26,11 @@ CREATE TABLE tasks
   movie_name INT REFERENCES movies(id)
 );
 
--- make a person
--- INSERT INTO people (person_name)
+-- make a movie
+INSERT INTO movies (movie_name, release_date) VALUES ('Django Unchained', '12/25/12');
+
+-- make a person_name
+INSERT INTO people (person_name, director, movie_name) VALUES ('Quentin Tarantino', true, 1);
+
+-- make a task
+INSERT INTO tasks (task_name, description, person_id, movie_name) VALUES ('Brad Pitt', 'get Brad Pitt to sign contract', 1, 1);
