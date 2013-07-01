e45613e348ac3a00033837cf4889aa6fc88d8732
Theodore Cleveland <tcclevela@gmail.com>
2013-07-01 16:40:39 -0400

    switch root route protocol to /todo route

--- main.rb
+++ main.rb
@@ -3,17 +3,20 @@ require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-helpers do 
-	def run_sql(sql)
-		db = PG.connect(dbname: 'production', host: 'localhost')
-		result = db.exec(sql)
-		db.close
-		result
-	end
+
+def run_sql(sql)
+	db = PG.connect(dbname: 'production', host: 'localhost')
+	result = db.exec(sql)
+	db.close
+	result
 end
 
 
 get '/' do
+
+end
+
+get '/tasks' do
 	sql = "SELECT * FROM tasks"
 	@tasks = run_sql(sql)
 	erb :todos
