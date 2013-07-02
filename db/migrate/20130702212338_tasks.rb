095729662cc8325da005566218bbc07e18f9491f
mvanger <mvanger@nyu.edu>
2013-07-02 17:33:08 -0400

    Edited other migration files

--- db/migrate/20130702212338_tasks.rb
+++ db/migrate/20130702212338_tasks.rb
@@ -1,5 +1,10 @@
 class Tasks < ActiveRecord::Migration
   def up
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.references :person
+      t.references :movie
   end
 
   def down
