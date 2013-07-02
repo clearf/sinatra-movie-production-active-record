3c4a9505ffa8be4571efcefde21e94e76db18908
Theodore Cleveland <tcclevela@gmail.com>
2013-07-02 17:00:22 -0400

    created migration for todos

--- db/migrate/20130702205725_tasks.rb
+++ db/migrate/20130702205725_tasks.rb
@@ -1,5 +1,14 @@
 class Tasks < ActiveRecord::Migration
   def up
+  	create_table :todos do |t|
+  		t.string :task
+  		t.string :details
+  		t.string :due
+  		t.boolean :urgent
+  		t.boolean :complete
+		t.references :person
+		t.references :movie
+	end
   end
 
   def down
