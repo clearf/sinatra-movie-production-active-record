1b320d88d5ddd7811cf001b82b34cbb94f6af098
mvanger <mvanger@nyu.edu>
2013-07-02 17:29:39 -0400

    Edited people migration file

--- db/migrate/20130702212315_people.rb
+++ db/migrate/20130702212315_people.rb
@@ -1,7 +1,12 @@
 class People < ActiveRecord::Migration
   def up
+    create_table :people do |t|
+      t.string :name
+      t.references :movie
+      t.references :task
   end
 
   def down
+    drop_table :people
   end
 end
