8be40c62ecdfcf9a8c62e25e14ab3bb2d294ebad
Beryl <beryl.schragger@gmail.com>
2013-07-02 20:48:53 -0400

    Add tables and classes.

--- main.rb
+++ main.rb
@@ -1,8 +1,25 @@
 require 'pg'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pry'
+require 'sinatra/activerecord'
 
+set :database, {adapter: 'postgresql',
+                database: 'production_new',
+                host: 'localhost'}
 
+class Person < ActiveRecord::Base
+  has_many :tasks
+end
+
+class Movie < ActiveRecord::Base
+  has_many :tasks
+end
+
+class Task < ActiveRecord::Base
+  belongs_to :person
+  belongs_to :movie
+end
 
 get '/new_todo'  do
 end
